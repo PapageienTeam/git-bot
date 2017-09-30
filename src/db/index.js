8ab19dc48b46a9aac0e478e020ae0dae54458765
Max J. Polster <max.polster@id-p.de>
2017-09-30 17:36:53 +0200

    Hopefully fixed much sql... please :(

--- src/db/index.js
+++ src/db/index.js
@@ -21,13 +21,17 @@ const validateEnum = (val, name, max) => {
 const db = module.exports = {
 	rel_types: ['assigned', 'created', 'mentioned', 'subscribed'],
 
-	clearAll: () => pool.query('TRUNCATE User, Issue, user_issue_rel'),
+	clearAll: async() => {
+		await pool.query('DELETE FROM "User"');
+		await pool.query('DELETE FROM "Issue"');
+		await pool.query('DELETE FROM user_issue_rel');
+	},
 
 	user: {
 		add: async({ github_login, slack_id }) => {
 			validateString(github_login, 'user.github_login');
 			validateString(slack_id, 'user.slack_id');
-			await pool.query('INSERT INTO User(github_login, slack_id) VALUES($1, $2)', [github_login, slack_id]);
+			await pool.query('INSERT INTO "User"(github_login, slack_id) VALUES($1, $2)', [github_login, slack_id]);
 		}
 	},
 
@@ -45,10 +49,10 @@ const db = module.exports = {
 			validateEnum(status, 1, 'issue.status');
 			validateString(title, 'issue.title');
 			validateString(url, 'issue.url');
-			await pool.query('INSERT INTO Issue(github_id, status, title, url) VALUES($1, $2, $3, $4)', [github_id, status, title, url]);
+			await pool.query('INSERT INTO "Issue"(github_id, status, title, url) VALUES($1, $2, $3, $4)', [github_id, status, title, url]);
 
 			async function addRel(user_github_login, type) {
-				let res = await pool.query('SELECT id FROM User WHERE github_login = $1', [user_github_login]);
+				let res = await pool.query('SELECT id FROM "User" WHERE github_login = $1', [user_github_login]);
 				if (res.rows.length > 0) {
 					let id = res.rows[0][0];
 					let typeId = db.rel_types.indexOf(type);
