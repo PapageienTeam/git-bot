5425ba5ada9e89dac1967fd31ec49884594cc724
Lena Kociemba <Lena@Zurs-MacBook-Pro.local>
2017-09-30 16:04:40 +0200

    Add test functions

--- src/slack-bot/slack-api-calls.js
+++ src/slack-bot/slack-api-calls.js
@@ -1,27 +1,52 @@
 'use strict';
 
+var bluebird = require('bluebird')
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-var bot_token = process.env.SLACK_BOT_TOKEN || '';
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-250269402038-63yphR6uHbdTlnktuzlLn6ub';
 var rtm = new RtmClient(bot_token);
 
-let channel;
+var channel = 0;
 
-function connect(){
+var connect = function (connecting_channel){
    // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-     for (const c of rtmStartData.channels) {
-   	  if (c.is_member && c.name ==='ki-github') {
-           channel = c.id
-        }
-     }
-     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+      for (const c of rtmStartData.channels) {
+         if (c.is_member && c.name === connecting_channel) {
+            channel = c.id
+         }
+      }
+      console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
    });
-
    // you need to wait for the client to fully connect before you can send messages
-   rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-     rtm.sendMessage("Hello!", channel);
+   var promise = new Promise(function(resolve){
+      rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+         console.log("Connected");
+      });
    });
-
    rtm.start();
+   return promise;
+};
+
+var send = function(message){
+   rtm.sendMessage(message, channel);
+};
+
+var receive = function(){
+   return new Promise( function( resolve ){
+      rtm.on(RTM_EVENTS.MESSAGE, resolve);
+   });
 }
+
+/*
+var receive = function(){
+   return new Promise( funtion (resolve){
+      rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+         console.log("Nachricht empfangen: ", message)
+         resolve(message)
+      });
+   });
+}*/
+
+module.exports = {connect: connect, send: send, receive: receive}
--- test/slack-api-calls_test.js
+++ test/slack-api-calls_test.js
@@ -1,5 +1,25 @@
 test('Slackverbindung testen', () => {
-   const sut = require ('../../src/slack-bot/slack-api-calls.js')
-   sut.connect("ki-github");
-	true;
-	});
+   const sut = require('../src/slack-bot/slack-api-calls');
+   sut.connect("ai-issuebot");
+   true;
+});
+
+test('Nachricht senden', () => {
+   const sut = require('../src/slack-bot/slack-api-calls');
+   return sut.connect("ai-issuebot").then(function(data){
+      expect.assertions(1);
+      sut.send("Hallo wie gehts?");
+   });
+});
+/*
+test('Nachricht empfangen', () => {
+   const sut = require('../src/slack-bot/slack-api-calls');
+   return sut.connect("ai-issuebot").then(function(data){
+      var promise = sut.receive().then(function (data){
+         expect.assertions(1);
+         console.log("Promisewert: ", data);
+      });
+      sut.send("Unit test");
+      return promise;
+   });
+});*/
